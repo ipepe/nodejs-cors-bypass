78761a84b0da2b84719d1df74fdc923e5913f5f0
Patryk Ptasinski <patryk.ptasinski@ipepe.pl>
2015-02-21 14:21:25 +0100

    Fixed cors requests and commeted server.js

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
 //nodejs-geodata-analitics-api
-//v.0.1.0
+var this_api_version = '0.1.1';
 
-//dependencies
+// ========== DEPENDENCIES
 var express = require('express');
 var app = express();
 var mmdbreader = require('maxmind-db-reader');
+var cors = require('cors');
+var app = express();
 
+//========== CONFIGURATION
 //this api is hosted at openshift.com
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+// ========== SETUP EXPRESS
 //openshift is serving node behind proxy
 app.set('trust proxy', process.env.OPENSHIFT_NODEJS_IP );
 
+// cors setup
+app.use( cors() );
+
+//json contentType
+app.use(function(req, res, next) {
+	res.contentType('application/json');
+	next();
+});
+
+//========== MAXMIND DATABASE SETUP
 //load database
 var geodataCity = mmdbreader.openSync('./GeoLite2-City.mmdb');
 
+// ========== MY APP VARIABLES
 //api statistic counter, for now until server restart...
 var apiHitCounter = {json:0, variable: 0, callback:0};
 
+//========== APP CODE
 function createApiResponse(req, api_type){
 	var api = {
-		api_name:'api.iPePe.pl WebAPI for Web analysis and statistics',
-		api_version:'0.0.1',
-		api_github:'https://github.com/ipepe/nodejs-geodata-analitics-api',
+		api_name: 'api.iPePe.pl WebAPI for Web analysis and statistics',
+		api_version: this_api_version,
+		api_github: 'https://github.com/ipepe/nodejs-geodata-analitics-api',
 		api_hit_count: apiHitCounter,
 		result: {}
 	};
@@ -45,22 +61,27 @@ function createApiResponse(req, api_type){
 }
 
 app.get('/', function (req, res) {
-	var response = {usage:{variable:"/var=variableName",json:"/api.json",callback:'/callback=functionName'}}
-	res.contentType('application/json');
+	var response = {
+		api_name: 'api.iPePe.pl WebAPI for Web analysis and statistics',
+		api_version: this_api_version,
+		api_github: 'https://github.com/ipepe/nodejs-geodata-analitics-api',
+		usage:{
+			variable:"/var=variableName",
+			json:"/api.json",
+			callback:'/callback=functionName'
+		}
+	}
 	res.send(JSON.stringify(response));
 });
 
 app.get('/api.json', function (req, res) {
-	res.contentType('application/json');
 	res.send(createApiResponse(req, 'json'));
 });
 
 app.get('/var=*', function (req, res) {
-	res.contentType('application/json');
 	res.send('var ' + req.params['0'] + ' = ' + createApiResponse(req, 'variable') );
 });
 app.get('/callback=*', function (req, res) {
-	res.contentType('application/json');
 	res.send( req.params['0'] + '(' + createApiResponse(req, 'callback') + ');' );
 });
 
