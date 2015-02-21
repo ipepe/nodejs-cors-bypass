ec8d4c545dc520b41afb8181eb9acab3ba7ab9e4
Patryk Ptasinski <patryk.ptasinski@ipepe.pl>
2015-02-21 14:37:42 +0100

    Refactored code, adjusted api name

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //nodejs-geodata-analitics-api
-var this_api_version = '0.1.1';
+var this_api_version = '0.1.2';
+var this_api_name = 'api.iPePe.pl WebAPI for WebClient analysis';
+var this_api_github = 'https://github.com/ipepe/nodejs-geodata-analitics-api';
 
 // ========== DEPENDENCIES
 var express = require('express');
@@ -29,48 +31,58 @@ app.use(function(req, res, next) {
 //========== MAXMIND DATABASE SETUP
 //load database
 var geodataCity = mmdbreader.openSync('./GeoLite2-City.mmdb');
+var geodata_info = { 
+	info:'This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com',
+	db_datetime:'2015-02-19T01:53:23.236Z',
+	db_unix_datetime:1424310803236
+};
 
 // ========== MY APP VARIABLES
 //api statistic counter, for now until server restart...
-var apiHitCounter = {json:0, variable: 0, callback:0};
+var apiHitCounter = {
+	json:0,
+	variable: 0,
+	callback:0
+};
 
 //========== APP CODE
 function createApiResponse(req, api_type){
 	var api = {
-		api_name: 'api.iPePe.pl WebAPI for Web analysis and statistics',
+		api_name: this_api_name,
 		api_version: this_api_version,
-		api_github: 'https://github.com/ipepe/nodejs-geodata-analitics-api',
+		api_github: this_api_github,
 		api_hit_count: apiHitCounter,
 		result: {}
 	};
 	api.api_hit_count[api_type]++;
 
-	api.result.ip = req.ip;
-	api.result.ips = req.ips;
-	api.result.useragent = req.headers['user-agent'];
-	api.result.language = req.headers['accept-language'];
-	api.result.referer =  req.headers.referrer || req.headers.referer;
-	
-	api.result.geodata = { 
-		info:'This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com',
-		db_datetime:'2015-02-19T01:53:23.236Z',
-		db_unix_datetime:1424310803236
+	api.result.ipepe = {
+		client_ip: req.ip,
+		client_proxy_chain_ips: req.ips,
+		client_useragent: req.headers['user-agent'],
+		client_language: req.headers['accept-language']
 	};
+	if( req.headers.referrer || req.headers.referer ){
+		api.result.ipepe.client_referer = req.headers.referrer || req.headers.referer;
+	};
+	
+	api.result.geodata = geodata_info;
+
 	api.result.geodata.result = geodataCity.getGeoDataSync(api.result.ip);
 	return JSON.stringify(api);
 }
 
 app.get('/', function (req, res) {
 	var response = {
-		api_name: 'api.iPePe.pl WebAPI for Web analysis and statistics',
+		api_name: this_api_name,
 		api_version: this_api_version,
-		api_github: 'https://github.com/ipepe/nodejs-geodata-analitics-api',
+		api_github: this_api_github,
 		usage:{
 			variable:"/var=variableName",
 			json:"/api.json",
 			callback:'/callback=functionName'
 		}
-	}
+	};
 	res.send(JSON.stringify(response));
 });
 
